31d236e000cc0c5d2fd54abdfdef79e039279599
Anas El Hajjaji <anas.elhajjaji@ticketmaster.com>
2022-12-15 10:59:47 -0500

    fix external ports

--- python-devcontainer/consumer.py
+++ python-devcontainer/consumer.py
@@ -4,7 +4,7 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
     'posts',
-    bootstrap_servers=['kafka:9093'],
+    bootstrap_servers=['kafka:9092'],
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
 # note that this for loop will block forever to wait for the next message
--- python-devcontainer/producer.py
+++ python-devcontainer/producer.py
@@ -4,7 +4,7 @@ import json
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers=['kafka:9093'],
+    bootstrap_servers=['kafka:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 producer.send('posts', {'author': 'anas', 'content': 'Hello Kafka!', 'created_at': datetime.now().isoformat()})
